b5c0d6366d12b6efa7ae28f471f23e7f6a88b930
tonyclem <tonyclem001@gmail.com>
2022-06-30 09:29:04 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cocktail DB
+
+## Application that fetch requests from this https://www.thecocktaildb.com/api/json/v1/1/lookup.php?
+
+### Cocktail drinks app.
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        lorem Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
+        massa egestas mollis varius; dignissim elementum. Mollis tincidunt
+        mattis hendrerit dolor eros enim, nisi ligula ornare. Hendrerit
+        parturient habitant pharetra rutrum gravida porttitor eros feugiat.
+        Mollis elit sodales taciti duis praesent id. Consequat urna vitae morbi
+        nunc congue.
+      </p>
+    </section>
+  );
+};
+
+export default About;
